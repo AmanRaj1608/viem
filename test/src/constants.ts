1c2002865c902bbd8c797e7487e672f42235a74f
jxom <7336481+jxom@users.noreply.github.com>
2025-02-21 18:32:47 +1100

    chore: up snap

--- test/src/constants.ts
+++ test/src/constants.ts
@@ -58,7 +58,8 @@ export const address = {
 export const poolId =
   Number(process.env.VITEST_POOL_ID ?? 1) *
     Number(process.env.VITEST_SHARD_ID ?? 1) +
-  (process.env.VITE_NETWORK_TRANSPORT_MODE === 'webSocket' ? 100 : 0)
+  (process.env.VITE_NETWORK_TRANSPORT_MODE === 'webSocket' ? 100 : 0) +
+  Math.floor(Math.random() * 10000)
 
 export const typedData = {
   basic: {
